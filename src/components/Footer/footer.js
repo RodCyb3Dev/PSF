a012287b9b709d144d89a545ea825bdeed2e5564
rodcode47 <rodney.hammad@gmail.com>
2018-06-30 23:23:58 +0300

    changed header name

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -10,7 +10,7 @@ const footer = () => (
             <img alt="PSF Logo" src="/images/nba_logo.png"/>
         </Link>
         <div className={styles.right}>
-        &#169; {CURRENT_YEAR} <b>SPF |</b> All Rights Reserved | Powered by  
+        &#169; {CURRENT_YEAR} <b>PSF |</b> All Rights Reserved | Powered by  
             <a className="badge badge-info ml-1" target="_new" title="Developers Blog" href="https://kodeflash.herokuapp.com/"> <big>Kodeflash</big></a>
         </div>
     </div>
